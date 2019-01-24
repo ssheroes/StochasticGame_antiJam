65169eabc83925aeeebb0534bf20657ea53a56de
ssheroes <zhaoxuw01@163.com>
2019-01-24 15:08:37 +0800

    correct the policy see
    correct the policy see function to see several states

--- testRFAntiJam.m
+++ testRFAntiJam.m
@@ -11,7 +11,7 @@ RandStream.setGlobalStream(s);
 
 numActions = 5;
 drawProbability = 0.01;
-StepCntTotal = 10000;
+StepCntTotal = 15000;
 decay = 10^(-2/StepCntTotal);
 expl = 0.2;
 gamma = 0.01;
@@ -23,12 +23,14 @@ gamma = 0.01;
 Channel = channel(1,8);
 Pu = Pu();
 RFAntiJam = RFAntiJamtester(Channel,Pu);
-
-playerB = RandomAntiJam();
+% playerB = RandomAntiJam();
+ playerB = minimaxAntiJam(decay,expl,gamma);
+% playerB = minimaxAntiJam(); 
 Attacker = Attacker(playerB,4);
 Com = Com(decay,expl,gamma);
 
-PolicySee = RFAntiJam.train(Com,Attacker,StepCntTotal);
+StateSee = [0,1,0,0;0,6,0,0;0,11,0,0];
+PolicySee = RFAntiJam.train(Com,Attacker,StepCntTotal,StateSee);
 figure;
 plot(PolicySee(42,:));
 
