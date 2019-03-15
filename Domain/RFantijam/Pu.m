a6e27330a54fde745d494729febf07c01297af6b
ssheroes <zhaoxuw01@163.com>
2019-03-15 12:24:31 +0800

    correcr PU
    correcr PU

--- Domain/RFantijam/Pu.m
+++ Domain/RFantijam/Pu.m
@@ -4,13 +4,17 @@ classdef Pu < handle
     properties
         PUnum;
         PuState;
-        ProbTran;   %P{1->0}=P{1->1}=P{0->0}=P{0->1}=0.5
+        ProbTran;   %[P{0->0},P{0->1};P{1->0},P{1->1}]
+        alpha;   %P{1->0} 
+        beta;    %P{0->1}
         PuStateSet;
     end
     
     methods
         function obj = Pu()
-            obj.ProbTran = [0.5,0.5];
+            alpha = 0.5;   %P{1->0}
+            beta = 0.5;    %P{0->1}
+            obj.ProbTran = [1-beta,beta;alpha,1-alpha];
             obj.PuStateSet = [0,1];
             obj.PuState = 0;
         end
@@ -20,9 +24,10 @@ classdef Pu < handle
         end
         
         function PuStateOut = StateVariate(obj)
+            stateIndex = obj.PuState+1;
             RandChoice = rand;
             choice = 1;
-            while RandChoice>sum(obj.ProbTran(1:choice))
+            while RandChoice>sum(obj.ProbTran(stateIndex,1:choice))
                 choice = choice + 1;
             end
             obj.PuState = obj.PuStateSet(choice);
